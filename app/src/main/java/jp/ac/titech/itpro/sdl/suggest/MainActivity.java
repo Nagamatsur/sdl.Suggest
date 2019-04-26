70d2d4ca4c02ade7719a2be08056d2cec551cfdf
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2019-04-26 09:30:42 +0900

    Use Class#getSimpleName

--- app/src/main/java/jp/ac/titech/itpro/sdl/suggest/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/suggest/MainActivity.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private final static String TAG = "MainActivity";
+    private final static String TAG = MainActivity.class.getSimpleName();
 
     private final static int MSG_RESULT = 1234;
 
